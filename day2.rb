b62c27ad6e84bd5b9c33a0e4f175ab1159c6e809
Charles Brian Quinn <me@seebq.com>
2018-12-02 13:01:04 -0500

    Day 2, more ruby

---
+++ day2.rb
@@ -0,0 +1,25 @@
+# --- Part Two ---
+
+frequency = 0
+frequencies_seen = Hash.new(0)
+frequency_seen_twice = false
+
+until frequency_seen_twice
+  File.foreach('day1_input.txt') do |line|
+    # the operator + or - is the first character:
+    operator = line[0]
+    # the calibration is the rest of it, converted to an integer
+    calibration = line[1, line.length].to_i
+    # just send (metaprogramming) it to perform the operation:
+    frequency = frequency.send(operator, calibration)
+    # store it:
+    frequencies_seen[frequency] += 1
+    # if we've seen a frequency twice, store it and break from loop
+    if frequencies_seen[frequency] == 2
+      frequency_seen_twice = frequency
+      break
+    end
+  end
+end
+
+puts frequency_seen_twice # 81204
